920567389d842de768c5a8e12aa6a9963ab98874
hanif365 <m.a.hanifkhan64@gmail.com>
2023-11-25 23:10:37 +0600

    fix responsive issue

--- src/pages/ShowUser/ShowUser.jsx
+++ src/pages/ShowUser/ShowUser.jsx
@@ -9,7 +9,6 @@ const ShowUser = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        // const response = await fetch("http://localhost:4000/api/users");
         const response = await fetch(
           "https://userrecord-backend.vercel.app/api/users"
         );
@@ -69,46 +68,48 @@ const ShowUser = () => {
     <div className="px-5 text-center bg-[#edf2fb] min-h-screen pt-28">
       <h1 className="text-2xl font-bold mb-4 py-5">User List</h1>
       <ToastContainer />
-      <table className="w-full border mb-20">
-        <thead>
-          <tr>
-            <th className="border p-2">Name</th>
-            <th className="border p-2">Sectors</th>
-            <th className="border p-2">Agree Terms</th>
-            <th className="border p-2">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr
-              key={user.id}
-              className="hover:bg-gray-100 transition duration-300"
-            >
-              <td className="border p-2">{user.name}</td>
-              <td className="border p-2">{user.sectors.join(", ")}</td>
-              <td className="border p-2">{user.agreeTerms ? "Yes" : "No"}</td>
-              <td className="border p-2 space-y-2 md:space-y-0">
-                <Link
-                  to={`/update/${user._id}`}
-                  className="bg-yellow-500 text-white px-2 py-1 rounded mr-2 hover:bg-yellow-600"
-                >
-                  Edit
-                </Link>
-                <button
-                  className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
-                  onClick={() => handleDelete(user._id)}
-                >
-                  Delete
-                </button>
-              </td>
+      <div className="overflow-x-auto">
+        <table className="w-full border mb-20">
+          <thead>
+            <tr>
+              <th className="border p-2">Name</th>
+              <th className="border p-2">Sectors</th>
+              <th className="border p-2">Agree Terms</th>
+              <th className="border p-2">Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr
+                key={user.id}
+                className="hover:bg-gray-100 transition duration-300"
+              >
+                <td className="border p-2">{user.name}</td>
+                <td className="border p-2">{user.sectors.join(", ")}</td>
+                <td className="border p-2">{user.agreeTerms ? "Yes" : "No"}</td>
+                <td className="border p-2 space-y-2 md:space-y-0">
+                  <Link
+                    to={`/update/${user._id}`}
+                    className="bg-yellow-500 text-white px-2 py-1 rounded mr-2 hover:bg-yellow-600 block md:inline-block"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 block md:inline-block"
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
       <Link
         to="/"
-        className={`py-3 px-10 rounded text-xl font-bold tracking-wider cursor-pointer transition-all ease-in-out bg-[#2196f3] text-white hover:bg-blue-600`}
+        className={`py-3 px-10 my-10 rounded text-xl font-bold tracking-wider cursor-pointer transition-all ease-in-out bg-[#2196f3] text-white hover:bg-blue-600 inline-block`}
       >
         Add More Data
       </Link>
